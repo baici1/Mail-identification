fb3828b620364a39735dd9dc18c0ec755ddee629
Osilly <55748410+Osilly@users.noreply.github.com>
2022-07-26 18:18:07 +0800

    Avoid renewal training to override model weights

--- bert/model.py
+++ bert/model.py
@@ -4,12 +4,18 @@ from transformers import BertConfig, BertModel
 
 
 class Bert(nn.Module):
-    def __init__(self, pre_train_model="chinese-bert-wwm", classific_num=2):
+    def __init__(
+        self, pre_train_model="chinese-bert-wwm", classific_num=2, resume=False
+    ):
         super().__init__()
         self.classific_num = classific_num
         config = BertConfig.from_pretrained(pre_train_model)
-        self.backbone = BertModel.from_pretrained(pre_train_model, config=config)
-        # fin-tune
+        # Avoid renewal training to override model weights
+        if resume:
+            self.backbone = BertModel(config=config)
+        else:
+            self.backbone = BertModel.from_pretrained(pre_train_model, config=config)
+        # Fine-tune
         for param in self.backbone.parameters():
             param.requires_grad = True
         pooler_fc_size = config.num_labels
